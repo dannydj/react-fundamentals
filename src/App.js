01a520ea38d0bebd601e1beab290f1f374f01b5d
Danny Medrano <dmedrano@devsu.com>
2019-04-01 15:02:17 -0500

    State

--- src/App.js
+++ src/App.js
@@ -1,61 +1,50 @@
 import React, { Component } from 'react';
 import './App.css';
 
-/** El componente que acabo de generar es un componente funcional, así como este, existe su alternativa basada en javascript llamado
- * componente de clase (cada una de estas alternativas tiene sus pro y sus contras, ademas de sus limitaciones)
+/** El estado "state" es un término común en programación. El estado de un elemento, componente u objeto
+ * Un programa es statefull, es decir, que puede guardar un estado cuando está configurado para RECORDAR los cambios, modificaciones y los eventos
+ * sufridos a lo largo de su uso.
+ * 
+ * En React, los componentes pueden guradar un estado interno para sus propiedades y las modificaciones que estas sufran. Esto permite
+ * actualizar la info que el usuario envíe, guardar datos que haya descargado, etc. 
+ * 
+ * El state es todo lo que puede sufrir una modificación dentro de un componente.
+ * 
+ * Las props NO se pueden modificar, pero lo que SI se puede modificar es el estado del componente.
+ * 
+ * Tanto componentes funcionales como de clase pueden manipular el estado. (A partir de react 16.8, antes solo podían los componentes de clase)
  */
-const MyComponent = () => (
-	<p>Hola</p>
-);
 
-/** El componente de clase debe heredar de la clase Component de la lib de react, lo que le distingue del componente funcional es que tiene 
- * un método render que retorna lo que debe mostrar este componente o lo que debe representar dentro de la página web
- */
-class MyClassComponent extends Component {
-	render() {
-		return <p>Hola soy de la clase</p>;
-	};
+class Contador extends Component {
+	/** El primer paso para que mi componente guarde un estado es que tengo que inicializar los valores iniciales del estado dentro del constructor.
+	 * A diferencia del componente funcional, el constructor es quien recibe las props
+	 */
+	constructor(props) {
+		// Parte del primer paso es: Enviamos las props al constructor del padre (es decir de la clase Component de la que hereda)
+		super(props);
+
+		// Inicializo el estado (es un objeto json)
+		this.state = {
+			contador: 0
+		};
+	}
+
+	aumentar = () => { this.setState({ contador: this.state.contador + 1 }) };
+
+	/** Para asignar un nuevo valor al estado se utiliza el método setState */
+	render = () => (
+		<div>
+			<p>{this.state.contador}</p>
+			<button onClick={this.aumentar}>Aumentar</button>
+		</div>
+	);
 }
 
-/** Cohesión: Formar un todo unido, donde no se distinguen las partes que lo conforman. En un sistema hay que evitar desarrollar sistemas que tengan
- * elementos que dependan entre ellos, es decir, debo desarrollar elementos independientes entre si, que NO exista cohesión entre ellos.
- * 
- * El desarrollo basado en componentes es útil en este caso, se busca que c/componente sea independiente entre si.
- * El siguiente ememplo rompe esta regla, es decir los componentes A y B utilizan la misma variable, lo cual está mal. Cada componente no debe utilizar
- * información externa a el mismo.
- */
-let name = 'Danny';
-
-const A = () => (<p>Hola {name}</p>);
-
-const B = () => (<p>{name}: 10</p>);
-
-/** Para evitar esto se utilizan las props. El uso de props permite que un componente padre pase el valor a sus componentes hijos.
- * Reescribiendo el anterior código, pero ahora utilizando props. En un componente funcional, las props se reciben como un ÚNICO argumento de la función.
- * Todas las propiedades que son enviadas al componente son "claves" o atributos del objeto props.
- * 
- * Los props son READ-ONLY, es decir, un componente no puede modificar sus propias props, solo las puede leer. Cuando el valor que se le pasa vía props
- * a un componente se actualiza o se modifica, el componente se actualiza automáticamente
- */
-const A2 = (props) => /* (<p>Hola {props.name}</p>) */ (props.children);
-
-const B2 = (props) => (<p>{props.name}: 10</p>);
-
-/** Para utilizar cualquiera componente (sea funcional o de clase) se lo incluye con <> */
 class App extends Component {
 	render() {
-		let name2 = 'Danny';
 		return (
 			<div>
-				<MyComponent />
-				<MyClassComponent />
-				<A />
-				<B />
-				<A2 name={name2} >
-					<p>Hola soy un componente hijo</p>
-					<p>Hola soy otro componente hijo</p>
-				</A2>
-				<B2 name={name2} />
+				<Contador />
 			</div>
 		);
 	}
