ecffdbe71338b3220696f77f7adf2ff23aef673e
Danny Medrano <dmedrano@devsu.com>
2019-04-01 15:54:56 -0500

    Working with forms

--- src/App.js
+++ src/App.js
@@ -1,50 +1,58 @@
 import React, { Component } from 'react';
 import './App.css';
 
-/** El estado "state" es un término común en programación. El estado de un elemento, componente u objeto
- * Un programa es statefull, es decir, que puede guardar un estado cuando está configurado para RECORDAR los cambios, modificaciones y los eventos
- * sufridos a lo largo de su uso.
- * 
- * En React, los componentes pueden guradar un estado interno para sus propiedades y las modificaciones que estas sufran. Esto permite
- * actualizar la info que el usuario envíe, guardar datos que haya descargado, etc. 
- * 
- * El state es todo lo que puede sufrir una modificación dentro de un componente.
- * 
- * Las props NO se pueden modificar, pero lo que SI se puede modificar es el estado del componente.
- * 
- * Tanto componentes funcionales como de clase pueden manipular el estado. (A partir de react 16.8, antes solo podían los componentes de clase)
- */
-
-class Contador extends Component {
-	/** El primer paso para que mi componente guarde un estado es que tengo que inicializar los valores iniciales del estado dentro del constructor.
-	 * A diferencia del componente funcional, el constructor es quien recibe las props
-	 */
+class MyForm extends Component {
 	constructor(props) {
-		// Parte del primer paso es: Enviamos las props al constructor del padre (es decir de la clase Component de la que hereda)
 		super(props);
 
-		// Inicializo el estado (es un objeto json)
 		this.state = {
-			contador: 0
+			email: '',
+			password: ''
 		};
 	}
 
-	aumentar = () => { this.setState({ contador: this.state.contador + 1 }) };
+	/** Lo que esta función hace es modificar el state cuando haya cambios en los inputs del formulario.
+	 * Con un mismo método se puede sincronizar cualquier cambio en cualquier control con cualquier propiedad del state
+	 */
+	syncChanges = (value, property) => {
+		let state = {};
+		state[property] = value;
+		this.setState(state);
+	};
 
-	/** Para asignar un nuevo valor al estado se utiliza el método setState */
-	render = () => (
-		<div>
-			<p>{this.state.contador}</p>
-			<button onClick={this.aumentar}>Aumentar</button>
-		</div>
-	);
+	submitForm = () => {
+		console.log(this.state);
+	}
+
+	/** El evento onChange tiene una funcion cuyo argumento es el evento (recibe el evento). En base al evento puedo obtener el valor del control (del input) es
+	 * a traves de target.value
+	 * 
+	 * ev.target.value se desgloza asi:
+	 * event: es un objeto que contiene toda clase de datos acerca del evento que acaba de dispararse
+	 * target: es una propiedad del evento que contiene el elemento que disparó ese evento o en el que suceció el evento
+	 * value: cotiene el valor del cambio que hizo el usuario (lo que ha escrito), lo toma de la propiedad value pero del input
+	 *  */
+	render() {
+		return (
+			<form>
+				<input onChange={(ev) => { this.syncChanges(ev.target.value, 'email') }}
+					type="email" value={this.state.email} placeholder="Email" />
+				<input onChange={(ev) => { this.syncChanges(ev.target.value, 'password') }}
+					type="password" value={this.state.password} placeholder="*****" />
+				<div>
+					<input onClick={this.submitForm}
+						type="submit" value="Iniciar sesión" />
+				</div>
+			</form>
+		);
+	}
 }
 
 class App extends Component {
 	render() {
 		return (
 			<div>
-				<Contador />
+				<MyForm />
 			</div>
 		);
 	}
