8f6a4896e73fe907b580a4a6470aa7edbb6a4d20
Danny Medrano <dmedrano@devsu.com>
2019-04-01 16:36:03 -0500

    Ajax requests

--- src/App.js
+++ src/App.js
@@ -2,22 +2,35 @@ import React, { Component } from 'react';
 import './App.css';
 
 class Blog extends Component {
+	/** Una buena práctica de programación es no delegar muchas tareas al constructor. El constructor NO debe estar cargado de código.
+	 * Mientras más código se le cargue al constructor, más dependiente se vuelve de otros, es más difícil de probar, más difícil de reemplazar, etc.
+	 * 
+	 * En React existen LOS MÉTODOS DEL CICLO DE VIDA, se tratan de métodos especiales que tiene un componente y se van a ejecutar en puntos claves del
+	 * ciclo de vida del componente
+	 *  */
 	constructor(props) {
 		super(props);
 		this.state = {
-			articles: [
-				'Mi primer coponente en React',
-				'Introducción a React',
-				'Qué es React'
-			]
+			articles: []
 		};
 	}
+
+	/** componentDidMount: Este es un método del ciclo de vida. Este método se ejecutará tan pronto el componente esté listo y se haya colocado en la interfaz (en la página web) */
+	componentDidMount = () => {
+		/** Encadeno promesas */
+		fetch('https://jsonplaceholder.typicode.com/posts')
+			.then(response => response.json())
+			.then(data => {
+				console.log(data);
+				this.setState({ articles: data })
+			});
+	};
 
 	render() {
 		return (
 			<div>
 				{
-					this.state.articles.map((title) => <p>{title}</p>)
+					this.state.articles.map((article) => <p key={article.id}>{article.title}</p>)
 				}
 			</div>
 		);
